f27f4bd97281f7c34528d4b8ca3cc4c782d7fb9a
Godspower Udokpoh <harshmallins@gmail.com>
2023-05-29 11:32:39 +0100

    Settings & Profile

---
+++ Hi5-Dynamic/static/scripts/profile.js
@@ -0,0 +1,14 @@
+function uploadProfilePicture() {
+    const fileInput = document.getElementById('profile-picture-input');
+    const file = fileInput.files[0];
+    
+    // Handle file upload logic (e.g., validate file type, size, and send to backend)
+  }
+  
+  function updateBio() {
+    const bioTextarea = document.getElementById('bio-textarea');
+    const newBio = bioTextarea.value;
+  
+    // Handle bio update logic (e.g., send updated bio to backend)
+  }
+  
